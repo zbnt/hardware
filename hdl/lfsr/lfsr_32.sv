c6d3ab2657b5fc09832b1f3f3941233e1f467d4c
Oscar Rojas <oscar@oscar-rc.dev>
2019-03-20 23:37:04 -0300

    [hdl] lfsr: Split in two modules

---
+++ hdl/lfsr/lfsr_32.sv
@@ -0,0 +1,17 @@
+
+// https://www.xilinx.com/support/documentation/application_notes/xapp052.pdf
+
+module lfsr_32 #(parameter init_state = 32'd0)
+(
+	input logic clk,
+	input logic rst,
+	output logic [31:0] value
+);
+	always_ff @(posedge clk or posedge rst) begin
+		if(rst) begin
+			value <= init_state;
+		end else begin
+			value <= {value[30:0], value[31] ^~ value[21] ^~ value[1] ^~ value[0]};
+		end
+	end
+endmodule
