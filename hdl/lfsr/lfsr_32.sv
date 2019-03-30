4e1a76a24735bf07c0e8371b0e0085d1a3fdb28c
Oscar Rojas <oscar@oscar-rc.dev>
2019-03-29 21:06:46 -0300

    [hdl] lfsr: Add enable input signal

--- hdl/lfsr/lfsr_32.sv
+++ hdl/lfsr/lfsr_32.sv
@@ -5,12 +5,13 @@ module lfsr_32 #(parameter init_state = 32'd0)
 (
 	input logic clk,
 	input logic rst,
+	input logic enable,
 	output logic [31:0] value
 );
 	always_ff @(posedge clk or posedge rst) begin
 		if(rst) begin
 			value <= init_state;
-		end else begin
+		end else if(enable) begin
 			value <= {value[30:0], value[31] ^~ value[21] ^~ value[1] ^~ value[0]};
 		end
 	end
--- hdl/lfsr/lfsr_64.sv
+++ hdl/lfsr/lfsr_64.sv
@@ -5,12 +5,13 @@ module lfsr_64 #(parameter init_state = 64'd0)
 (
 	input logic clk,
 	input logic rst,
+	input logic enable,
 	output logic [63:0] value
 );
 	always_ff @(posedge clk or posedge rst) begin
 		if(rst) begin
 			value <= init_state;
-		end else begin
+		end else if(enable) begin
 			value <= {value[62:0], value[63] ^~ value[62] ^~ value[60] ^~ value[59]};
 		end
 	end
