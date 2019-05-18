81b5cfbc162362555aee8b85397f3785c8acdda4
Oscar Rojas <oscar@oscar-rc.dev>
2019-05-18 06:10:24 -0400

    [hdl] counter: Add up and down input signals

--- hdl/counter.sv
+++ hdl/counter.sv
@@ -3,13 +3,21 @@ module counter #(parameter width = 32)
 (
 	input logic clk,
 	input logic rst,
+
+	input logic up,
+	input logic down,
+
 	output logic [width-1:0] count
 );
 	always_ff @(posedge clk or posedge rst) begin
 		if(rst) begin
 			count <= '0;
 		end else begin
-			count <= count + 'd1;
+			if(up & ~down) begin
+				count <= count + 'd1;
+			end else if(~up & down) begin
+				count <= count - 'd1;
+			end
 		end
 	end
 endmodule
