c398b2d06c52123d044375f97dbf0ad849efdf2c
Oscar Rojas <oscar@oscar-rc.dev>
2019-07-04 13:30:53 -0400

    [hdl] sync_ffs: Add ASYNC_REG attribute, add input register

--- hdl/sync_ffs.sv
+++ hdl/sync_ffs.sv
@@ -1,19 +1,28 @@
 
 module sync_ffs #(parameter width, parameter stages = 2)
 (
-	input logic clk,
+	input logic clk_src,
+	input logic clk_dst,
 	input logic [width-1:0] data_in,
 	output logic [width-1:0] data_out
 );
-	logic [width-1:0] sync_stages[0:stages-1];
-	genvar i;
+	(* ASYNC_REG = "TRUE" *) logic [width-1:0] sync_stages[0:stages-1];
+	logic [width-1:0] sync_stages_in;
 
-	always_ff @(posedge clk) begin
-		sync_stages[0] <= data_in;
+	// source clock domain
+
+	always_ff @(posedge clk_src) begin
+		sync_stages_in <= data_in;
 	end
 
-	for(i = 1; i < stages; ++i) begin
-		always_ff @(posedge clk) begin
+	// destination clock domain
+
+	always_ff @(posedge clk_dst) begin
+		sync_stages[0] <= sync_stages_in;
+	end
+
+	for(genvar i = 1; i < stages; ++i) begin
+		always_ff @(posedge clk_dst) begin
 			sync_stages[i] <= sync_stages[i-1];
 		end
 	end
