7f138c946b8b21ae715e8e73f8b87b9c6ba2fd64
Oscar Rojas <oscar@oscar-rc.dev>
2019-07-10 05:18:48 -0400

    [cores] eth_frame_detector: Update comment with documentation

--- cores/eth_frame_detector/hdl/eth_frame_detector.sv
+++ cores/eth_frame_detector/hdl/eth_frame_detector.sv
@@ -42,8 +42,55 @@
 		\reg FD_CFG: Frame detector configuration register.
 			\access RW
 
-			\field SRST   0      Software reset, active high, must be set back to 0 again manually.
-			\field EN     1-6    Pattern matching enable.
+			\field EN     0      Global enable, set to 1 in order to enable frame detection.
+			\field SRST   1      Software reset, active high, must be set back to 0 again manually.
+			\field ENA1   2      Enable pattern A1.
+			\field ENA2   3      Enable pattern A2.
+			\field ENA3   4      Enable pattern A3.
+			\field ENB1   5      Enable pattern B1.
+			\field ENB2   6      Enable pattern B2.
+			\field ENB3   7      Enable pattern B3.
+
+		\reg FD_FIFO_OCCUP: FIFO occupancy.
+			\access RO
+
+			\field FOCCUP 0-10   Number of entries currently stored in the FIFO.
+
+		\reg FD_FIFO_POP: Read values from FIFO.
+			\access RW
+
+			\field FPOP   0-31   If set to a value different from 0, read the next set of values from the FIFO and store them in
+			                     the registers. If read, always returns 0.
+
+		\reg FD_TIME_L: Detection time, lower half.
+			\access RO
+
+			\field TIMEL  0-31   Time of the last detection, lower 32 bits.
+
+		\reg FD_TIME_H: Detection time, upper half.
+			\access RO
+
+			\field TIMEH  0-31   Time of the last detection, upper 32 bits.
+
+		\reg FD_MATCHED: Matched patterns.
+			\access RO
+
+			\field MA1    0      Set to 1 if a match for pattern A1 was detected.
+			\field MA2    1      Set to 1 if a match for pattern A2 was detected.
+			\field MA3    2      Set to 1 if a match for pattern A3 was detected.
+			\field MB1    3      Set to 1 if a match for pattern B1 was detected.
+			\field MB2    4      Set to 1 if a match for pattern B2 was detected.
+			\field MB3    5      Set to 1 if a match for pattern B3 was detected.
+
+		\mem PATTERN_MEM_A: Patterns for frames going from A to B.
+			\access RW
+			\at    0x2000
+			\size  3072
+
+		\mem PATTERN_MEM_B: Patterns for frames going from B to A.
+			\access RW
+			\at    0x4000
+			\size  3072
 */
 
 module eth_frame_detector
