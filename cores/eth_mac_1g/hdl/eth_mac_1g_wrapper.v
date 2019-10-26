9a7439486cb8ce30f8080e254068ea09080f64c6
Oscar Rojas <oscar@oscar-rc.dev>
2019-10-26 00:05:24 -0300

    [cores] eth_mac_1g: Allow using gtx_clk as TX and RX clock

--- cores/eth_mac_1g/hdl/eth_mac_1g_wrapper.v
+++ cores/eth_mac_1g/hdl/eth_mac_1g_wrapper.v
@@ -4,7 +4,7 @@
 	file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
-module eth_mac_1g_w #(parameter iface_type = "GMII", parameter family_name = "7-Series", parameter use_clk90 = 0)
+module eth_mac_1g_w #(parameter C_IFACE_TYPE = "GMII", parameter C_CLK_INPUT_STYLE = "BUFR", parameter C_USE_CLK90 = 0, parameter C_GTX_AS_RX_CLK = 0)
 (
 	// Clock signals
 
@@ -60,7 +60,7 @@ module eth_mac_1g_w #(parameter iface_type = "GMII", parameter family_name = "7-
 	output wire gmii_tx_en,
 	output wire gmii_tx_er
 );
-	if(iface_type == "RGMII") begin
+	if(C_IFACE_TYPE == "RGMII") begin
 		wire [3:0] rgmii_rd_delayed;
 		wire rgmii_rx_ctl_delayed;
 
@@ -68,8 +68,8 @@ module eth_mac_1g_w #(parameter iface_type = "GMII", parameter family_name = "7-
 		#(
 			.TARGET("XILINX"),
 			.IODDR_STYLE("IODDR"),
-			.CLOCK_INPUT_STYLE(family_name != "7-Series" ? "BUFG" : "BUFR"),
-			.USE_CLK90(use_clk90 ? "TRUE" : "FALSE"),
+			.CLOCK_INPUT_STYLE(C_CLK_INPUT_STYLE),
+			.USE_CLK90(C_USE_CLK90 ? "TRUE" : "FALSE"),
 			.ENABLE_PADDING(1),
 			.MIN_FRAME_LENGTH(64)
 		)
@@ -134,49 +134,96 @@ module eth_mac_1g_w #(parameter iface_type = "GMII", parameter family_name = "7-
 		assign gmii_tx_clk = 1'b0;
 		assign gmii_tx_en = 1'b0;
 		assign gmii_tx_er = 1'b0;
-	end else if(iface_type == "GMII") begin
-		eth_mac_1g_gmii
-		#(
-			.TARGET("XILINX"),
-			.IODDR_STYLE("IODDR"),
-			.CLOCK_INPUT_STYLE(family_name != "7-Series" ? "BUFG" : "BUFR"),
-			.ENABLE_PADDING(1),
-			.MIN_FRAME_LENGTH(64)
-		)
-		eth_mac_1g_gmii_inst
-		(
-			.gtx_clk(gtx_clk),
-			.gtx_rst(~gtx_rst_n),
-			.rx_clk(rx_clk),
+	end else if(C_IFACE_TYPE == "GMII") begin
+		if(~C_GTX_AS_RX_CLK) begin
+			eth_mac_1g_gmii
+			#(
+				.TARGET("XILINX"),
+				.IODDR_STYLE("IODDR"),
+				.CLOCK_INPUT_STYLE(C_CLK_INPUT_STYLE),
+				.ENABLE_PADDING(1),
+				.MIN_FRAME_LENGTH(64)
+			)
+			eth_mac_1g_gmii_inst
+			(
+				.gtx_clk(gtx_clk),
+				.gtx_rst(~gtx_rst_n),
+				.rx_clk(rx_clk),
 
-			.tx_axis_tdata(tx_axis_tdata),
-			.tx_axis_tvalid(tx_axis_tvalid),
-			.tx_axis_tready(tx_axis_tready),
-			.tx_axis_tlast(tx_axis_tlast),
-			.tx_axis_tuser(tx_axis_tuser),
+				.tx_axis_tdata(tx_axis_tdata),
+				.tx_axis_tvalid(tx_axis_tvalid),
+				.tx_axis_tready(tx_axis_tready),
+				.tx_axis_tlast(tx_axis_tlast),
+				.tx_axis_tuser(tx_axis_tuser),
 
-			.rx_axis_tdata(rx_axis_tdata),
-			.rx_axis_tvalid(rx_axis_tvalid),
-			.rx_axis_tlast(rx_axis_tlast),
-			.rx_axis_tuser(rx_axis_tuser),
+				.rx_axis_tdata(rx_axis_tdata),
+				.rx_axis_tvalid(rx_axis_tvalid),
+				.rx_axis_tlast(rx_axis_tlast),
+				.rx_axis_tuser(rx_axis_tuser),
 
-			.gmii_rx_clk(gmii_rx_clk),
-			.gmii_rxd(gmii_rxd),
-			.gmii_rx_dv(gmii_rx_dv),
-			.gmii_rx_er(gmii_rx_er),
-			.mii_tx_clk(mii_tx_clk),
-			.gmii_tx_clk(gmii_tx_clk),
-			.gmii_txd(gmii_txd),
-			.gmii_tx_en(gmii_tx_en),
-			.gmii_tx_er(gmii_tx_er),
+				.gmii_rx_clk(gmii_rx_clk),
+				.gmii_rxd(gmii_rxd),
+				.gmii_rx_dv(gmii_rx_dv),
+				.gmii_rx_er(gmii_rx_er),
+				.mii_tx_clk(mii_tx_clk),
+				.gmii_tx_clk(gmii_tx_clk),
+				.gmii_txd(gmii_txd),
+				.gmii_tx_en(gmii_tx_en),
+				.gmii_tx_er(gmii_tx_er),
 
-			.tx_error_underflow(tx_error),
-			.rx_error_bad_frame(rx_error[0]),
-			.rx_error_bad_fcs(rx_error[1]),
-			.speed(speed),
+				.tx_error_underflow(tx_error),
+				.rx_error_bad_frame(rx_error[0]),
+				.rx_error_bad_fcs(rx_error[1]),
+				.speed(speed),
 
-			.ifg_delay(8'd12)
-		);
+				.ifg_delay(8'd12)
+			);
+		end else begin
+			eth_mac_1g #(
+				.ENABLE_PADDING(1),
+				.MIN_FRAME_LENGTH(64)
+			)
+			eth_mac_1g_inst
+			(
+				.tx_clk(gtx_clk),
+				.tx_rst(~gtx_rst_n),
+				.rx_clk(gtx_clk),
+				.rx_rst(~gtx_rst_n),
+
+				.tx_axis_tdata(tx_axis_tdata),
+				.tx_axis_tvalid(tx_axis_tvalid),
+				.tx_axis_tready(tx_axis_tready),
+				.tx_axis_tlast(tx_axis_tlast),
+				.tx_axis_tuser(tx_axis_tuser),
+
+				.rx_axis_tdata(rx_axis_tdata),
+				.rx_axis_tvalid(rx_axis_tvalid),
+				.rx_axis_tlast(rx_axis_tlast),
+				.rx_axis_tuser(rx_axis_tuser),
+
+				.gmii_rxd(gmii_rxd),
+				.gmii_rx_dv(gmii_rx_dv),
+				.gmii_rx_er(gmii_rx_er),
+				.gmii_txd(gmii_txd),
+				.gmii_tx_en(gmii_tx_en),
+				.gmii_tx_er(gmii_tx_er),
+
+				.rx_clk_enable(1'b1),
+				.tx_clk_enable(1'b1),
+				.rx_mii_select(1'b0),
+				.tx_mii_select(1'b0),
+
+				.tx_error_underflow(tx_error),
+				.rx_error_bad_frame(rx_error[0]),
+				.rx_error_bad_fcs(rx_error[1]),
+
+				.ifg_delay(8'd12)
+			);
+
+			assign speed = 2'd0;
+			assign rx_clk = gtx_clk;
+			assign gmii_tx_clk = gtx_clk;
+		end
 
 		assign rgmii_td = 4'd0;
 		assign rgmii_txc = 1'b0;
--- cores/eth_mac_1g/xgui/eth_mac_1g_v1_0.tcl
+++ cores/eth_mac_1g/xgui/eth_mac_1g_v1_0.tcl
@@ -7,63 +7,99 @@ proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
   #Adding Page
   set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
-  ipgui::add_param $IPINST -name "family_name" -parent ${Page_0} -widget comboBox
-  ipgui::add_param $IPINST -name "iface_type" -parent ${Page_0} -widget comboBox
-  set use_clk90 [ipgui::add_param $IPINST -name "use_clk90" -parent ${Page_0}]
-  set_property tooltip {Output the TX clock with a 2ns delay} ${use_clk90}
+  ipgui::add_param $IPINST -name "C_IFACE_TYPE" -parent ${Page_0} -widget comboBox
+  #Adding Group
+  set Interface_options [ipgui::add_group $IPINST -name "Interface options" -parent ${Page_0}]
+  ipgui::add_param $IPINST -name "C_CLK_INPUT_STYLE" -parent ${Interface_options} -widget comboBox
+  set C_USE_CLK90 [ipgui::add_param $IPINST -name "C_USE_CLK90" -parent ${Interface_options}]
+  set_property tooltip {Add a 2ns delay to the TX clock output} ${C_USE_CLK90}
+  ipgui::add_param $IPINST -name "C_GTX_AS_RX_CLK" -parent ${Interface_options}
+
 
 
 }
 
-proc update_PARAM_VALUE.use_clk90 { PARAM_VALUE.use_clk90 PARAM_VALUE.iface_type } {
-	# Procedure called to update use_clk90 when any of the dependent parameters in the arguments change
+proc update_PARAM_VALUE.C_CLK_INPUT_STYLE { PARAM_VALUE.C_CLK_INPUT_STYLE PARAM_VALUE.C_GTX_AS_RX_CLK } {
+	# Procedure called to update C_CLK_INPUT_STYLE when any of the dependent parameters in the arguments change
 	
-	set use_clk90 ${PARAM_VALUE.use_clk90}
-	set iface_type ${PARAM_VALUE.iface_type}
-	set values(iface_type) [get_property value $iface_type]
-	if { [gen_USERPARAMETER_use_clk90_ENABLEMENT $values(iface_type)] } {
-		set_property enabled true $use_clk90
+	set C_CLK_INPUT_STYLE ${PARAM_VALUE.C_CLK_INPUT_STYLE}
+	set C_GTX_AS_RX_CLK ${PARAM_VALUE.C_GTX_AS_RX_CLK}
+	set values(C_GTX_AS_RX_CLK) [get_property value $C_GTX_AS_RX_CLK]
+	if { [gen_USERPARAMETER_C_CLK_INPUT_STYLE_ENABLEMENT $values(C_GTX_AS_RX_CLK)] } {
+		set_property enabled true $C_CLK_INPUT_STYLE
 	} else {
-		set_property enabled false $use_clk90
+		set_property enabled false $C_CLK_INPUT_STYLE
 	}
 }
 
-proc validate_PARAM_VALUE.use_clk90 { PARAM_VALUE.use_clk90 } {
-	# Procedure called to validate use_clk90
+proc validate_PARAM_VALUE.C_CLK_INPUT_STYLE { PARAM_VALUE.C_CLK_INPUT_STYLE } {
+	# Procedure called to validate C_CLK_INPUT_STYLE
 	return true
 }
 
-proc update_PARAM_VALUE.family_name { PARAM_VALUE.family_name } {
-	# Procedure called to update family_name when any of the dependent parameters in the arguments change
+proc update_PARAM_VALUE.C_GTX_AS_RX_CLK { PARAM_VALUE.C_GTX_AS_RX_CLK PARAM_VALUE.C_IFACE_TYPE } {
+	# Procedure called to update C_GTX_AS_RX_CLK when any of the dependent parameters in the arguments change
+	
+	set C_GTX_AS_RX_CLK ${PARAM_VALUE.C_GTX_AS_RX_CLK}
+	set C_IFACE_TYPE ${PARAM_VALUE.C_IFACE_TYPE}
+	set values(C_IFACE_TYPE) [get_property value $C_IFACE_TYPE]
+	if { [gen_USERPARAMETER_C_GTX_AS_RX_CLK_ENABLEMENT $values(C_IFACE_TYPE)] } {
+		set_property enabled true $C_GTX_AS_RX_CLK
+	} else {
+		set_property enabled false $C_GTX_AS_RX_CLK
+	}
 }
 
-proc validate_PARAM_VALUE.family_name { PARAM_VALUE.family_name } {
-	# Procedure called to validate family_name
+proc validate_PARAM_VALUE.C_GTX_AS_RX_CLK { PARAM_VALUE.C_GTX_AS_RX_CLK } {
+	# Procedure called to validate C_GTX_AS_RX_CLK
 	return true
 }
 
-proc update_PARAM_VALUE.iface_type { PARAM_VALUE.iface_type } {
-	# Procedure called to update iface_type when any of the dependent parameters in the arguments change
+proc update_PARAM_VALUE.C_USE_CLK90 { PARAM_VALUE.C_USE_CLK90 PARAM_VALUE.C_IFACE_TYPE } {
+	# Procedure called to update C_USE_CLK90 when any of the dependent parameters in the arguments change
+	
+	set C_USE_CLK90 ${PARAM_VALUE.C_USE_CLK90}
+	set C_IFACE_TYPE ${PARAM_VALUE.C_IFACE_TYPE}
+	set values(C_IFACE_TYPE) [get_property value $C_IFACE_TYPE]
+	if { [gen_USERPARAMETER_C_USE_CLK90_ENABLEMENT $values(C_IFACE_TYPE)] } {
+		set_property enabled true $C_USE_CLK90
+	} else {
+		set_property enabled false $C_USE_CLK90
+	}
 }
 
-proc validate_PARAM_VALUE.iface_type { PARAM_VALUE.iface_type } {
-	# Procedure called to validate iface_type
+proc validate_PARAM_VALUE.C_USE_CLK90 { PARAM_VALUE.C_USE_CLK90 } {
+	# Procedure called to validate C_USE_CLK90
+	return true
+}
+
+proc update_PARAM_VALUE.C_IFACE_TYPE { PARAM_VALUE.C_IFACE_TYPE } {
+	# Procedure called to update C_IFACE_TYPE when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_IFACE_TYPE { PARAM_VALUE.C_IFACE_TYPE } {
+	# Procedure called to validate C_IFACE_TYPE
 	return true
 }
 
 
-proc update_MODELPARAM_VALUE.iface_type { MODELPARAM_VALUE.iface_type PARAM_VALUE.iface_type } {
+proc update_MODELPARAM_VALUE.C_IFACE_TYPE { MODELPARAM_VALUE.C_IFACE_TYPE PARAM_VALUE.C_IFACE_TYPE } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.iface_type}] ${MODELPARAM_VALUE.iface_type}
+	set_property value [get_property value ${PARAM_VALUE.C_IFACE_TYPE}] ${MODELPARAM_VALUE.C_IFACE_TYPE}
 }
 
-proc update_MODELPARAM_VALUE.family_name { MODELPARAM_VALUE.family_name PARAM_VALUE.family_name } {
+proc update_MODELPARAM_VALUE.C_USE_CLK90 { MODELPARAM_VALUE.C_USE_CLK90 PARAM_VALUE.C_USE_CLK90 } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.family_name}] ${MODELPARAM_VALUE.family_name}
+	set_property value [get_property value ${PARAM_VALUE.C_USE_CLK90}] ${MODELPARAM_VALUE.C_USE_CLK90}
 }
 
-proc update_MODELPARAM_VALUE.use_clk90 { MODELPARAM_VALUE.use_clk90 PARAM_VALUE.use_clk90 } {
+proc update_MODELPARAM_VALUE.C_CLK_INPUT_STYLE { MODELPARAM_VALUE.C_CLK_INPUT_STYLE PARAM_VALUE.C_CLK_INPUT_STYLE } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
-	set_property value [get_property value ${PARAM_VALUE.use_clk90}] ${MODELPARAM_VALUE.use_clk90}
+	set_property value [get_property value ${PARAM_VALUE.C_CLK_INPUT_STYLE}] ${MODELPARAM_VALUE.C_CLK_INPUT_STYLE}
+}
+
+proc update_MODELPARAM_VALUE.C_GTX_AS_RX_CLK { MODELPARAM_VALUE.C_GTX_AS_RX_CLK PARAM_VALUE.C_GTX_AS_RX_CLK } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_GTX_AS_RX_CLK}] ${MODELPARAM_VALUE.C_GTX_AS_RX_CLK}
 }
 
