fb309cbc866d863a7b1cc25626eab7d5e3cf1fdc
Oscar Rojas <oscar@oscar-rc.dev>
2019-11-12 08:23:35 -0300

    [projects] all: Reset the current run before trying to regenerate an out of date bitstream

--- projects/zedboard/dual_tgen_latency/build_bitstream.tcl
+++ projects/zedboard/dual_tgen_latency/build_bitstream.tcl
@@ -18,6 +18,7 @@ if { ![file exists vivado/zbnt_hw_dual_tgen_latency.xpr ] } {
 }
 
 if { [get_property needs_refresh [get_runs impl_1]] || [get_property status [get_runs impl_1]] == "Not started" } {
+	reset_runs synth_1
 	launch_runs impl_1 -to_step write_bitstream -jobs $jobs
 	wait_on_run impl_1
 }
