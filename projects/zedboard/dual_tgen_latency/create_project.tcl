5966ab42b9acb60a802f25d03b34e50633f6db7e
Oscar Rojas <oscar@oscar-rc.dev>
2022-11-22 20:20:05 -0300

    [projects] all: Set properties for IP synthesis runs

--- projects/zedboard/dual_tgen_latency/create_project.tcl
+++ projects/zedboard/dual_tgen_latency/create_project.tcl
@@ -22,18 +22,25 @@ update_ip_catalog -rebuild
 source bd_dual_tgen_latency.tcl
 read_verilog [make_wrapper -top -files [get_files bd_dual_tgen_latency.bd]]
 
-# Create synthesis run
+# Create IP synthesis runs
 
-set_property -name "flow" -value "Vivado Synthesis 2022" -objects [get_runs synth_1]
-set_property -name "strategy" -value "Flow_PerfOptimized_high" -objects [get_runs synth_1]
-set_property -name "steps.synth_design.args.fsm_extraction" -value "one_hot" -objects [get_runs synth_1]
-set_property -name "steps.synth_design.args.keep_equivalent_registers" -value "1" -objects [get_runs synth_1]
-set_property -name "steps.synth_design.args.resource_sharing" -value "off" -objects [get_runs synth_1]
-set_property -name "steps.synth_design.args.no_lc" -value "1" -objects [get_runs synth_1]
-set_property -name "steps.synth_design.args.shreg_min_size" -value "5" -objects [get_runs synth_1]
+generate_target all $bd
+export_ip_user_files -of_objects $bd -no_script -sync -force -quiet
+create_ip_run $bd
+
+# Configure synthesis
+
+set runs [get_runs *synth_1]
+set_property -name "flow" -value "Vivado Synthesis 2022" -objects $runs
+set_property -name "strategy" -value "Flow_PerfOptimized_high" -objects $runs
+set_property -name "steps.synth_design.args.fsm_extraction" -value "one_hot" -objects $runs
+set_property -name "steps.synth_design.args.keep_equivalent_registers" -value "1" -objects $runs
+set_property -name "steps.synth_design.args.resource_sharing" -value "off" -objects $runs
+set_property -name "steps.synth_design.args.no_lc" -value "1" -objects $runs
+set_property -name "steps.synth_design.args.shreg_min_size" -value "5" -objects $runs
 current_run -synthesis [get_runs synth_1]
 
-# Create implementation run
+# Configure implementation
 
 set_property -name "flow" -value "Vivado Implementation 2022" -objects [get_runs impl_1]
 set_property -name "strategy" -value "Performance_ExploreWithRemap" -objects [get_runs impl_1]
